215a87e0937c2399177f66351c616b8f8d22d9e6
j1wooldridge <jacques.wooldridge@gmail.com>
2019-10-10 14:36:53 +0000

    Add updated source changed since 2019-10-10 09:36

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,5 +3,9 @@ class PlacesController < ApplicationController
     def index
         @places = Place.order("name").page(params[:page]).per(5)    
     end
+    
+    def new 
+        @place = Place.new              
+    end
 
 end 
