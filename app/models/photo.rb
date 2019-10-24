2f47bf12a30e7ba5ef8a08282684ebfc5e8b8f7d
j1wooldridge <jacques.wooldridge@gmail.com>
2019-10-24 14:49:50 +0000

    Add updated source changed up until 2019-10-24 09:50

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,8 @@
-class Photo < ApplicationRecord
-  belongs_to :user  
-  belongs_to :place 
-end
+#class Photo < ApplicationRecord
+#  belongs_to :user  
+#  belongs_to :place 
+#end
 
 class Photo < ActiveRecord::Base
-  mount_uploader :picture, PictureUploader
+  mount_uploader :picture, PictureUploader  
 end 
